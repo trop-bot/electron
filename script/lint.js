87b4436468f9f3d92efec3bf5b112a53f4acfb46
Shelley Vohr <shelley.vohr@gmail.com>
2018-11-14 09:27:37 -0800

    address feedback from review

--- script/lint.js
+++ script/lint.js
@@ -15,6 +15,7 @@ const BLACKLIST = new Set([
   ['atom', 'browser', 'resources', 'win', 'resource.h'],
   ['atom', 'browser', 'ui', 'cocoa', 'atom_menu_controller.h'],
   ['atom', 'browser', 'ui', 'cocoa', 'atom_ns_window.h'],
+  ['atom', 'browser', 'ui', 'cocoa', 'atom_access_controller.h'],
   ['atom', 'browser', 'ui', 'cocoa', 'atom_ns_window_delegate.h'],
   ['atom', 'browser', 'ui', 'cocoa', 'atom_preview_item.h'],
   ['atom', 'browser', 'ui', 'cocoa', 'atom_touch_bar.h'],
